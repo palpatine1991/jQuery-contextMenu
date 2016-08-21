54b16f9ec5dc4d1b3cd03f5f938a09ce38c4e2ce
Bjorn Brala <bjorn@swis.nl>
2016-08-21 15:24:16 +0200

    Basic submenu test

--- test/specs/basictest.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-
-describe('webdriver.io page', function() {
-    it('should have the right title - the fancy generator way', function () {
-        browser.url('http://localhost/jQuery-contextMenu/test/integration/html/sub-menus.html');
-        var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
-    });
-});
---
+++ test/specs/submenu.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+
+describe('Test submenus', function() {
+    it('should navigate to submenu 2 levels deep and see correct alert for charlie', function () {
+        browser.url('http://localhost/jQuery-contextMenu/test/integration/html/sub-menus.html');
+        var title = browser.getTitle();
+        browser.rightClick('.context-menu-one');
+        browser.moveToObject('span=Sub group')
+        browser.moveToObject('span=Sub group 2')
+        browser.click('span=charlie')
+        assert.equal(browser.alertText(), 'clicked: fold2-key3');
+    });
+});
